d956bc2773e9fefde33d60351efcceaae48713b5
Alex Thoreux <alex.thoreux@gmail.com>
2018-08-13 16:54:17 +0200

    seeded 10 offers for 1 user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Offer.destroy_all
+User.destroy_all
+
+titles = ["Speakers", "DJ table", "Lights", "Smoke machine", "Beer tap"]
+categories = ["Sound", "Lights", "Alcohol", "Decorations", "Misc"]
+
+user1 = User.create(username: "user")
+
+10.times do
+  offer = Offer.new(title: titles.sample, price: rand(1..100), location: Faker::Address.full_address, category: categories.sample)
+  offer.user = user1
+  offer.save
+end
