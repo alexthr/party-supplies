08d54253724e9cf98bb0bfb136413115886d31b5
Carine LE CHARLES <carine.le.charles@gmail.com>
2018-08-13 17:36:30 +0200

    generate the User model with the devise generator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :offers, dependent: :destroy
   has_many :bookings
   validates :username, presence: true, uniqueness: true
