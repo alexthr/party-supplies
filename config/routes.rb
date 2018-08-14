94cec8098ea2ac4e33463ff27d31a1757da78c0d
Carine LE CHARLES <carine.le.charles@gmail.com>
2018-08-14 12:54:15 +0200

    menu navbar custo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'offers#index'
   resources :offers do
     resources :bookings, only: [:index, :new, :create]
+  end
+  resources :users, only: [:show]
+  namespace :my do
+    resources :offers, only: :index
   end
 end
