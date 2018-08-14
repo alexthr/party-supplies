7f0de6c5ed70ca1124e0c42a2691c9728ebc93fc
Carine LE CHARLES <carine.le.charles@gmail.com>
2018-08-14 17:37:40 +0200

    Add search bar

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,4 +2,8 @@ class Offer < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   validates :title, :price, :location, :category, presence: true
+
+  def self.search(search)
+    where("title LIKE ?", "%#{search}%")
+  end
 end
