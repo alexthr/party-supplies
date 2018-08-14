16a56294e0075b473561e8ed24be5c4b1ee6e140
Alex Thoreux <alex.thoreux@gmail.com>
2018-08-14 17:11:49 +0200

    new, create, edit, update work, need more design

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -9,17 +9,35 @@ class OffersController < ApplicationController
   end
 
   def new
+    @offer = Offer.new
+    @categories = ["Sound", "Lights", "Drinks", "Decorations", "Costumes", "Entertainment", "Misc"];
   end
 
   def create
+    @offer = Offer.new(offer_params)
+    @offer.user = current_user
+    @offer.save
+    redirect_to offer_path(@offer)
   end
 
   def edit
+    @offer = Offer.find(params[:id])
   end
 
   def update
+    @offer = Offer.find(params[:id])
+    @offer.update(offer_params)
+    redirect_to offer_path(@offer)
   end
 
   def destroy
+
   end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :description, :price, :category, :location, :photo)
+  end
+
 end
