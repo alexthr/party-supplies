3de44b4024422daa44e904c52c7e24c50a2bb211
Carine LE CHARLES <carine.le.charles@gmail.com>
2018-08-15 11:22:08 +0200

    Add order display to order filter by username

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,7 @@ class SearchController < ApplicationController
       @offers = Offer.where("title ILIKE ? OR category ILIKE ? OR location ILIKE ? OR description ILIKE ?",
         "%#{params[:q]}%", "%#{params[:q]}%", "%#{params[:q]}%", "%#{params[:q]}%").all.order("created_at DESC")
       if @offers.first.nil?
-        @offers = Offer.joins(:user).where("username ILIKE ?", "%#{params[:q]}%")
+        @offers = Offer.joins(:user).where("username ILIKE ?", "%#{params[:q]}%").all.order("created_at DESC")
       end
     else
       @offers = Offer.all.order("created_at DESC")
