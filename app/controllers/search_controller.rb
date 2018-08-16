6d69a45f9d4a7816b1b9ace3185d06dadb23c9b0
Carine LE CHARLES <carine.le.charles@gmail.com>
2018-08-16 15:49:20 +0200

    add gem pg-search. add number of offers found

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,7 @@
 class SearchController < ApplicationController
   def search
-    if params[:q]
-      @offers = Offer.where("title ILIKE ? OR category ILIKE ? OR location ILIKE ? OR description ILIKE ?",
-        "%#{params[:q]}%", "%#{params[:q]}%", "%#{params[:q]}%", "%#{params[:q]}%").all.order("created_at DESC")
-      if @offers.first.nil?
-        @offers = Offer.joins(:user).where("username ILIKE ?", "%#{params[:q]}%").all.order("created_at DESC")
-      end
+    if params[:query]
+      @offers=  Offer.search_offers(params[:query])
     else
       @offers = Offer.all.order("created_at DESC")
     end
