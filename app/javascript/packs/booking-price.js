ec22dc729716af0a0464fec1591db55925b41443
Alex Thoreux <alex.thoreux@gmail.com>
2018-08-17 12:06:51 +0200

    moved javascript code to a separate js file

--- app/javascript/packs/booking-price.js
+++ app/javascript/packs/booking-price.js
@@ -1,11 +1,32 @@
-const startDate = document.getElementById("booking_start_date");
-const endDate = document.getElementById("booking_end_date");
+let startDateInput = document.getElementById("booking_start_date");
+let endDateInput = document.getElementById("booking_end_date");
+let days = 0;
+let total = document.getElementById("js-booking-total");
+let price = total.dataset.price;
+console.log(total.innerHTML);
+console.log(price);
 
-console.log(startDate);
+startDateInput.addEventListener("change", (e) => {
+  let startDate = Date.parse(startDateInput.value);
+  let endDate = Date.parse(endDateInput.value);
+  days = calculateDays(startDate, endDate);
+  let totalPrice = price * days;
+  console.log(totalPrice);
+  total.innerHTML = `Total: ${totalPrice}€`;
+});
 
-startDate.addEventListener("change", (e) => {
-  console.log(startDate.value);
+endDateInput.addEventListener("change", (e) => {
+  let startDate = Date.parse(startDateInput.value);
+  let endDate = Date.parse(endDateInput.value);
+  days = calculateDays(startDate, endDate);
+  let totalPrice = price * days;
+  console.log(totalPrice);
+  total.innerHTML = `Total: ${totalPrice}€`;
 });
 
 
 
+function calculateDays(start, end) {
+  const diff = end - start;
+  return diff / (1000 * 60 * 60 * 24);
+}
