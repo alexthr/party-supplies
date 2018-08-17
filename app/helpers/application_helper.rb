11368c6286549078f72c194c53f7b77a2dbb5075
yannicksanchez <yannick.sanchez.guasch@gmail.com>
2018-08-17 15:21:42 +0200

    added a condition if you uploaded an avatar or not

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def cl_image_tag_or_not(instance_of_photo_uploader)
+    if instance_of_photo_uploader.present?
+      instance_of_photo_uploader
+    else
+      "default-avatar"
+    end
+  end
 end
